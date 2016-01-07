bc2090a8c2477a0a675ce7fcc73ce7259edb830a
hverlin <hugues.verlin@gmail.com>
2016-01-07 14:12:11 +0100

    fix xml detail restaurant

--- app/GFaim/app/src/main/java/fr/insa/h4401/gfaim/DetailsRestaurantFragment.java
+++ app/GFaim/app/src/main/java/fr/insa/h4401/gfaim/DetailsRestaurantFragment.java
@@ -9,6 +9,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.graphics.drawable.DrawableCompat;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
